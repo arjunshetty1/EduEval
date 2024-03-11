c142bd4f59ddaa4b9cd46b4d84db7d3529b4a2b7
arjunshetty1 <arjunjayapura@mail.com>
2024-03-11 11:49:35 +0530

    ewrewr

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,38 +4,42 @@ const multer = require("multer");
 const axios = require("axios");
 const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "../uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
 
-router.post("/paperdata", upload.single("file"), async function (req, res) {
-  const { question, answerkey } = req.body;
-  const file = req.file;
-  console.log("Form client", question);
-  console.log(answerkey);
-  console.log(file);
+router.post(
+  "/paperdata",
+  // upload.single("file"),
+  async function (req, res) {
+    const { question, answerkey } = req.body;
+    const file = req.file;
+    console.log("Form client", question);
+    console.log(answerkey);
+    console.log(file);
 
-  try {
-    // const imageData = fs.readFileSync(`./uploads/${file.filename}`);
-    // const response = await axios.post(
-    //   "https://edueval-pyserver-o0my6mg1o-arjun-shetty.vercel.app/receive-image",
-    //   {
-    //     image: imageData,
-    //     imageName: file.originalname,
-    //   }
-    // );
-    // console.log("from fs", imageData);
-    // console.log(response);
-    res.send("Data recived sucessfully");
-  } catch (error) {
-    console.log(error);
+    try {
+      // const imageData = fs.readFileSync(`./uploads/${file.filename}`);
+      // const response = await axios.post(
+      //   "https://edueval-pyserver-o0my6mg1o-arjun-shetty.vercel.app/receive-image",
+      //   {
+      //     image: imageData,
+      //     imageName: file.originalname,
+      //   }
+      // );
+      // console.log("from fs", imageData);
+      // console.log(response);
+      res.send("Data recived sucessfully");
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 module.exports = router;
