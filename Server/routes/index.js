1dcc0efd7d09cf7896d684138a796f5392821446
arjunshetty1 <arjunjayapura@mail.com>
2024-03-12 15:46:49 +0530

    Update

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,3 +1,43 @@
+// const express = require("express");
+// const router = express.Router();
+// const multer = require("multer");
+// const axios = require("axios");
+
+// const upload = multer();
+
+// router.post("/paperdata", upload.single("file"), async function (req, res) {
+//   const { question, answerkey } = req.body;
+//   const file = req.file;
+
+//   try {
+//     const response = await axios.post(
+//       "https://flask-demo1-gules.vercel.app/receive-image",
+//       {
+//         image: file.buffer, // Pass the image buffer directly
+//         imageName: file.originalname,
+//         question: question,
+//         answerkey: answerkey,
+//       },
+
+//       {
+//         headers: { "Content-Type": "multipart/form-data" },
+//       }
+//     );
+//     console.log(response.data);
+//     res.send({
+//       message: "Data received successfully",
+//       pyResponse: response.data,
+//     });
+//   } catch (error) {
+//     console.error("Error sending image:", error);
+//     res.status(500).send("Error sending image to server");
+//   }
+// });
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
@@ -10,13 +50,18 @@ router.post("/paperdata", upload.single("file"), async function (req, res) {
   const file = req.file;
 
   try {
+    const formData = new FormData();
+    formData.append("image", file.buffer, file.originalname);
+    formData.append("question", question);
+    formData.append("answerkey", answerkey);
+
     const response = await axios.post(
       "https://flask-demo1-gules.vercel.app/receive-image",
+      formData,
       {
-        image: file.buffer, // Pass the image buffer directly
-        imageName: file.originalname,
-        question: question,
-        answerkey: answerkey,
+        headers: {
+          "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+        },
       }
     );
     console.log(response.data);
@@ -31,3 +76,4 @@ router.post("/paperdata", upload.single("file"), async function (req, res) {
 });
 
 module.exports = router;
+
