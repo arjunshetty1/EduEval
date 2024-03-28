57b760a2a1418d4c84c770c272d0c915b3c6c013
arjunshetty1 <arjunjayapura@mail.com>
2024-03-28 23:23:51 +0530

    Update: New endpoint added

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -60,7 +60,7 @@ router.post(
         }
       }
 
-      const response = await axios.post("http://localhost:3002/receive-image", {
+      const response = await axios.post("http://127.0.0.1:5000/receive-image", {
         requestData,
         usn,
       });
